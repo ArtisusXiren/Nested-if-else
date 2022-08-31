bb799a3c13941a43897fcb53982f242a287488ec
ArtisusXiren <95497283+ArtisusXiren@users.noreply.github.com>
2022-08-31 22:32:07 +0530

    Add files via upload

---
+++ reels1 (1).py
@@ -0,0 +1,23 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+z=input("What do you like to listen?")
+if(z=="something good"):
+    print(" ")
+    print("O O","SUBAH DHUNDA..PUKARA TUJHE")
+    print(" V ") 
+    for i in range(1,4,1): 
+      for x in range(1,i,1):
+         print("- -","SHAAM")
+         print(" V ")
+    print(" ")
+    print("- -","SHAAM")
+    print(" V ")
+    print("K0")
+    for i in range(1,4,1): 
+      for x in range(1,i,1):
+         print("- -","SHAAM")
+         print(" V ")
+    print(" ")
+    print("- -","SHAAM")
+    print(" V ")
+    print("K0")
